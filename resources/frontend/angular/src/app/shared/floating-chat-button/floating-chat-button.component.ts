f7cd19c50d677e791f883903fcd9fa7cdeb3cce0
jayparmar <infinitie.jay@gmail.com>
2025-06-07 12:12:58 +0530

    removed the chat button from chat screen

--- resources/frontend/angular/src/app/shared/floating-chat-button/floating-chat-button.component.ts
+++ resources/frontend/angular/src/app/shared/floating-chat-button/floating-chat-button.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ChatPopupComponent } from '../chat-popup/chat-popup.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-floating-chat-button',
   templateUrl: './floating-chat-button.component.html',
   styleUrls: ['./floating-chat-button.component.scss']
 })
-export class FloatingChatButtonComponent implements OnInit {
+export class FloatingChatButtonComponent implements OnInit, OnDestroy {
   isChatOpen = false;
   dialogRef: any;
+  isOnChatPage = false;
+  private routerSubscription: Subscription;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private router: Router) { }
 
   ngOnInit(): void {
+    // Check initial route
+    this.isOnChatPage = this.router.url === '/chat';
+
+    // Subscribe to route changes
+    this.routerSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.isOnChatPage = event.url === '/chat';
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
   }
 
   /**
