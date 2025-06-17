8b286d16f3b5be9b06467ec4c6ade304360c3caa
jayparmar <infinitie.jay@gmail.com>
2025-06-17 11:19:05 +0530

    google translate added

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -147,6 +147,16 @@
             box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05) !important;
             border-radius: 8px !important;
         }
+
+        /* Hide Google Translate top bar */
+        .skiptranslate {
+            display: none !important;
+        }
+
+        /* Hide Google Translate iframe */
+        iframe.skiptranslate {
+            display: none !important;
+        }
     </style>
 </body>
 </html>
